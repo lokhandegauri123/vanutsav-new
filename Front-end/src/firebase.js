fd33065b0aadb8b320bbc67421ff5d3412f84c42
Gauri Lokhande <157399853+lokhandegauri123@users.noreply.github.com>
2026-01-04 15:59:47 +0530

    Fix Firebase env variable names

--- Front-end/src/firebase.js
+++ Front-end/src/firebase.js
@@ -10,10 +10,10 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  databaseURL:import.meta.env.VITE_FIREBASE_DATABASEURL
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getDatabase(app);
